ef81288c2229df6767f1cfa2da9d71a20a7e004e
usamaabusnena <148998536+usamaabusnena@users.noreply.github.com>
2025-01-15 08:14:02 +0300

    Update dijkstra_serial.c

--- dijkstra_serial.c
+++ dijkstra_serial.c
@@ -69,7 +69,6 @@ void dijkstra_serial(int V, int *adjacency_matrix, int *len, int *temp_distance)
     clock_t start = clock(); /* Records the start time for measuring the execution time */
 
     /* Computing the All Pairs Shortest Paths (APSP) in the graph */
-    // CHANGE TO SOURCE < 1 FOR SINGLE SOURCE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     for(int source = 0; source < V; source++){
     
         for (int i = 0; i < V; i++) /* Initialize vars arrays to current source */
@@ -106,7 +105,55 @@ void dijkstra_serial(int V, int *adjacency_matrix, int *len, int *temp_distance)
     /* Records the end time for measuring the execution time */
     clock_t end = clock();
     float seconds = (float)(end - start) / CLOCKS_PER_SEC;
-    printf("TOTAL ELAPSED TIME ON CPU = %f SECS\n", seconds);
+    printf("TOTAL TIME FOR DIJKSTRA ALL PAIRS SEARCH ON CPU = %f SECS\n", seconds);
+
+    free(visited);
+}
+
+void dijkstra_serial_single_source(int V, int *adjacency_matrix, int *len, int *temp_distance)
+{
+    boolean *visited = (boolean *)malloc(V * sizeof(boolean));
+
+    clock_t start = clock(); /* Records the start time for measuring the execution time */
+
+    /* Computing the All Pairs Shortest Paths (APSP) in the graph */
+    for(int source = 0; source < 1; source++){
+    
+        for (int i = 0; i < V; i++) /* Initialize vars arrays to current source */
+        {
+            visited[i] = FALSE;
+            temp_distance[i] = INFNTY;
+            len[source * V + i] = INFNTY;
+        }
+
+        len[source * V + source] = 0; /* Set the distance of the source vertex as 0 */
+
+        for (int count = 0; count < V - 1; count++)
+        {
+            /* Finds the vertex with the minimum distance from the current source vertex */
+            int current_vertex = find_min_distance(V, len + source * V, visited);
+
+            visited[current_vertex] = TRUE;
+
+            for (int v = 0; v < V; v++)
+            {
+                int weight = adjacency_matrix[current_vertex * V + v];
+                if (!visited[v] && weight && len[source * V + current_vertex] != INFNTY &&
+                    len[source * V + current_vertex] + weight < len[source * V + v])
+                {
+                    /* Updating the distance is beneficial */
+                    len[source * V + v] = len[source * V + current_vertex] + weight;
+                    temp_distance[v] = len[source * V + v];
+                }
+            }
+        }
+    }
+    
+
+    /* Records the end time for measuring the execution time */
+    clock_t end = clock();
+    float seconds = (float)(end - start) / CLOCKS_PER_SEC;
+    printf("TOTAL TIME FOR DIJKSTRA SINGLE SOURCE SEARCH ON CPU = %f SECS\n", seconds);
 
     free(visited);
 }
@@ -134,7 +181,7 @@ int main(int argc, char **argv)
     clock_t end = clock();
     float seconds = (float)(end - start) / CLOCKS_PER_SEC;
     printf("TIME TO CREATE GRAPH ON CPU = %f SECS\n", seconds);
-
+    dijkstra_serial_single_source(V, adjacency_matrix, len, temp_distance);
     dijkstra_serial(V, adjacency_matrix, len, temp_distance);
 
     /* print_adjacency_matrix(V, adjacency_matrix); */
